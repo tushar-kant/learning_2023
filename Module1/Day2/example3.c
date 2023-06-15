6d8b1cb275398ee3e1deb7ab582baa72bd88f3d6
tushar-kant <ece.190410029@silicon.ac.in>
2023-06-15 09:47:18 +0530

    day2

---
+++ Module1/Day2/example3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+    printf("size of the array");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("elements of the array\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < size; i += 2) {
+        sum += arr[i];
+    }
+    printf("sum%d\n", sum);
+
+    return 0;
+}
---
+++ Module1/Day2/example5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int countsetbit(int num) {
+    int count = 0;
+    while (num != 0) {
+        count += num & 1;
+        num >>= 1;
+    }
+    return count;
+}
+
+int funtotalbit(int arr[], int size) {
+    int totalBits = 0;
+    for (int i = 0; i < size; i++) {
+        totalBits += countsetbit(arr[i]);
+    }
+    return totalBits;
+}
+
+int main() {
+    int a[3] = {0x1, 0xF4, 0x10001};
+    int size = sizeof(a) / sizeof(a[0]);
+    int totalbits = funtotalbit(a, size);
+    printf("total%d\n", totalbits);
+
+    return 0;
+}
